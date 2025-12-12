11a827a5f77d3c5a94dee3bd2b1ba80326f18e0c
rcski77 <rcski77@gmail.com>
2025-12-12 16:10:38 -0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Bebas_Neue } from "next/font/google";
 import "./globals.css";
@@ -34,6 +35,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${bebasNeue.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
