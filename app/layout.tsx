60fd1d647e9f53ec108cd94b2f4fc82947bb3a12
rcski77 <rcski77@gmail.com>
2025-12-12 16:12:51 -0500

    added speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Bebas_Neue } from "next/font/google";
 import "./globals.css";
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
